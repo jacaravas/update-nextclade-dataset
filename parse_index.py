19faa9b2dc5151170ba0408550971c6661a7ead1
Jason Caravas <48955152+jacaravas@users.noreply.github.com>
2022-03-02 14:56:39 -0500

    First Commit
    Upload existing files

---
+++ parse_index.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+### Written by Jason Caravas, mqi4 
+### Identify most recent sars-cov-2 build
+
+import os, sys, re, json, argparse
+
+def main(arguments):
+    parser = argparse.ArgumentParser(description='Extract latest version for dataset')
+    parser.add_argument('-n','--name',type=str,help='Dataset name')
+    args = parser.parse_args()
+    
+    desiredDatasetName = args.name
+    
+    baseUrl = "https://data.clades.nextstrain.org"
+    
+    jsonString = ""
+    for line in sys.stdin:
+        jsonString += line
+
+    index = json.loads(jsonString)
+
+    for target in index["datasets"]:
+        name = target["name"]
+        if name == desiredDatasetName:
+            for datasetRefs in target["datasetRefs"]:
+                for version in datasetRefs["versions"]:
+                    if version["latest"]:
+                        tag = version["tag"]
+                        reference = target["defaultRef"]
+                        versionUrl = baseUrl + "/datasets/" + desiredDatasetName + "/references/" + reference + "/versions/" + tag+ "/files"
+                        print (versionUrl)
+                        sys.exit(0)
+    
+if __name__=='__main__':
+        main(sys.argv[1:])
---
+++ update_nextclade_dataset.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## J. Caravas with S. Shepard
+
+##  This script automates the nextclade update process
+
+index_url="https://data.clades.nextstrain.org/index.json"
+data_files=("genemap.gff" "primers.csv" "qc.json" "reference.fasta" "sequences.fasta" "tag.json" "tree.json" "virus_properties.json")
+
+set -xo
+
+while getopts n:o: flag
+do
+    case "${flag}" in
+        n) name=${OPTARG};;
+        o) outputdir=${OPTARG};;
+    esac
+done
+
+function check_status (){
+    exit_status=$?
+    if [[ $exit_status != 0 ]]; then
+        fatal_error
+    fi
+}
+
+function fatal_error () {
+    echo -e "Failed at $STAGE.  Terminating"
+    exit 1
+}
+
+STAGE="DOWNLOAD INDEX"
+file_base_url=$(curl -v $index_url | zcat | workflow/snakemake_rules/parse_index.py --name $name)
+check_status
+echo "Base URL = $file_base_url"
+
+STAGE="DOWNLOAD DATASET"
+if [ ! -d $outputdir ]; then
+    mkdir $outputdir
+fi
+for file in "${data_files[@]}"
+do
+    file_url=$file_base_url/$file
+    dest_file=$outputdir/$file
+    curl $file_url | zcat > $dest_file
+    check_status
+done
+
+
+
+
